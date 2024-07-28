c58f9f56b3029e3c9b5a1c34ba676e5e89f5aaa6
tatsv <tatsvdev@gmail.com>
2024-07-28 11:04:31 +0300

    initial commit

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,11 @@
+public class VariablesAndDataTypes {
+    public static void main (String[]args){
+        int myInt=10;
+        double myDouble=20;
+        String myString= "Hello World";
+        System.out.println("Integer"+ myInt);
+        System.out.println("Double"+ myDouble);
+        System.out.println("String"+ myString);
+         
+    }
+}
