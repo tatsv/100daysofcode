93c002dcd0987847fbe6a4660d56ba8890835ea3
tatsv <tatsvdev@gmail.com>
2024-07-30 02:51:14 +0300

    initial commit

---
+++ BasicArithmetic.java
@@ -0,0 +1,19 @@
+public class BasicArithmetic {
+    public static void main (String[] args){
+        
+        int num1=10;
+        int num2=5;
+
+        int addition = num1 + num2;
+        int substraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+        
+        System.out.println("Addition:" +addition);
+        System.out.println("Substraction:" +substraction);
+        System.out.println("Multiplication:" +multiplication);
+        System.out.println("Division:" +division);
+
+    }
+    
+}
